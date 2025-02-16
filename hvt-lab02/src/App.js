dbd42da298bd1d2dc73d44d0686749eea49e5fdf
AN-MBANK <mastershining000@gmail.com>
2025-02-16 15:52:03 +0700

    htv-lab02

---
+++ hvt-lab02/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Thông tin cá nhân</h1>
+        <p>
+          <strong>Họ và tên:</strong> mai bình an
+        </p>
+        <p>
+          <strong>Mã sinh viên:</strong> 2310900001
+        </p>
+        <p>
+          <strong>Ngày sinh:</strong> 03/01/2005
+        </p>
+        <p>
+          <strong>Điện thoại:</strong> 0394772786
+        </p>
+        <p>
+          <strong>Tên lớp:</strong> K23CNT1
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
